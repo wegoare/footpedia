e7d6d4ec9111abafe914e32d295d7dfe6685728e
wegoare <mayankgurjar1570@gmail.com>
2025-01-24 11:50:03 +0530

    Added .env and updated Firebase configuration

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCHT4a_9kr3x52jel_iZz82oTRiKKsJQgw",
-  authDomain: "footpedia-123.firebaseapp.com",
-  projectId: "footpedia-123",
-  storageBucket: "footpedia-123.appspot.com",
-  messagingSenderId: "1011567403706",
-  appId: "1:1011567403706:web:603336bf98a21553c2d883"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
