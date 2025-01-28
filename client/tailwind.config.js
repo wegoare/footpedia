61306d8a3ae2bfb0042db4aa26d9a05a74d6f804
wegoare <mayankgurjar1570@gmail.com>
2025-01-29 02:13:04 +0530

    3

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin';
+
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
-    require('tailwind-scrollbar'),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.example-class': {
+          color: 'red',
+        },
+      });
+    }),
   ],
 };
